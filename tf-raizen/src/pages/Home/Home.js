7b19babc09700cd3cdd1532fccd9147bb1851d95
Laura T Neumann <62152335+lathne@users.noreply.github.com>
2021-11-09 15:18:43 -0300

    tabela de tickets

--- tf-raizen/src/pages/Home/Home.js
+++ tf-raizen/src/pages/Home/Home.js
@@ -1,10 +1,9 @@
 import {useEffect} from 'react'
-import gsap, {Power3} from 'gsap';
-// import Title from '../../components/Title/Title'
-import WelcomeSection from '../../components/WelcomeSection/WelcomeSection';
-import Footer from '../../components/Footer/Footer';
+import {Link} from 'react-router-dom'
+import gsap, {Power3} from 'gsap'
+import SearchSection from '../../components/SearchSection/SearchSection';
+import Footer from '../../components/Footer/Footer'
 import './Home.scss'
-import placeholder from '../../images/placeholder.png'
 
 function Home() {
   let info = gsap.timeline()
@@ -15,48 +14,78 @@ function Home() {
 
     return (
       <>
-         <main className="container">
-            <section className="flex-items">
-              <WelcomeSection />
+         <main className="home-container">
+            <section>
+              <SearchSection />
             </section>
-            <section className="flex-items">
-              <div className="message-section">
-                <div className="message">
-                  <p>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."</p>
-                </div>   
-                <div>
-                  <img className="illustration" src={placeholder}/>
-                </div>  
+
+            <section className="grid-container">
+              <div className="grid-items">
+                <Link className="links" to="/">
+                  <img src="" alt="icon"/>
+                </Link>
+              </div>   
+              <div className="grid-items">
+                <Link className="links" to="/">
+                  <img src="" alt="icon"/>
+                </Link>
               </div>
-              {/* <div class="search">
-                <h1>Como podemos ajudar?</h1>
-                <input 
-                  type="text" 
-                  className="search-input"
-                  placeholder="Search"
-                />
-              </div> */}
+              <div className="grid-items">
+                <Link className="links" to="/">
+                  <img src="" alt="icon"/>
+                </Link>
+              </div>
+              <div className="grid-items">
+                <Link className="links" to="/">
+                  <img src="" alt="icon"/>
+                </Link>
+              </div>
+              <div className="grid-items">
+                <Link className="links" to="/">
+                  <img src="" alt="icon"/>
+                </Link>
+              </div>
+              <div className="grid-items">
+                <Link className="links" to="/">
+                  <img src="" alt="icon"/>
+                </Link>
+              </div>
+              <div className="grid-items">
+                <Link className="links" to="/">
+                  <img src="" alt="icon"/>
+                </Link>
+              </div>
+              <div className="grid-items">
+                <Link className="links" to="/">
+                  <img src="" alt="icon"/>
+                </Link>
+              </div>  
+              <div className="grid-items">
+                <Link className="links" to="/">
+                  <img src="" alt="icon"/>
+                </Link>
+              </div>
             </section>
 
             <section className= "flex-container">
               <div className="boxes">
-                Loren Ipsum
                 <button>Loren</button>
+                Loren Ipsum
               </div>
               <div className="boxes">
-                Loren Ipsum
                 <button>Loren</button>
+                Loren Ipsum
               </div>
               <div className="boxes">
-                Loren Ipsum
                 <button>Loren</button>
+                Loren Ipsum
               </div>
               <div className="boxes">
-                Loren Ipsum
                 <button>Loren</button>
+                Loren Ipsum
               </div>
             </section>
-          <Footer />
+            <Footer />
         </main>
       </>
     )
