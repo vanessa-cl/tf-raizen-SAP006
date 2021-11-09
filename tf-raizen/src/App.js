eb30f1fb706835f0fdcc40171b786396d4eab37e
Laura T Neumann <62152335+lathne@users.noreply.github.com>
2021-11-09 00:03:00 -0300

    home page

--- tf-raizen/src/App.js
+++ tf-raizen/src/App.js
@@ -1,19 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import MyTickets from './pages/MyTickets/MyTickets';
-import './App.scss';
+import './global.scss';
 import Header from './components/Header/Header';
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <main className="container">
+   
         <Routes>
           <Route path="/" exact element={<Home />} />
           <Route path="/mytickets" element={<MyTickets />} />
         </Routes>
-      </main>
+
     </BrowserRouter>
   );
 }
--- tf-raizen/src/pages/Home/Home.js
+++ tf-raizen/src/pages/Home/Home.js
@@ -1,7 +1,10 @@
 import {useEffect} from 'react'
 import gsap, {Power3} from 'gsap';
-import Title from '../../components/Title/Title'
-
+// import Title from '../../components/Title/Title'
+import WelcomeSection from '../../components/WelcomeSection/WelcomeSection';
+import Footer from '../../components/Footer/Footer';
+import './Home.scss'
+import placeholder from '../../images/placeholder.png'
 
 function Home() {
   let info = gsap.timeline()
@@ -12,16 +15,49 @@ function Home() {
 
     return (
       <>
-        <article className="page">
-          <section className="inner">
-            <Title lineContent="Centro de Serviços"/>
-          </section>
-          <section>
-            <p className="info">
-        
-            </p>
-          </section>  
-        </article>
+         <main className="container">
+            <section className="flex-items">
+              <WelcomeSection />
+            </section>
+            <section className="flex-items">
+              <div className="message-section">
+                <div className="message">
+                  <p>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."</p>
+                </div>   
+                <div>
+                  <img className="illustration" src={placeholder}/>
+                </div>  
+              </div>
+              {/* <div class="search">
+                <h1>Como podemos ajudar?</h1>
+                <input 
+                  type="text" 
+                  className="search-input"
+                  placeholder="Search"
+                />
+              </div> */}
+            </section>
+
+            <section className= "flex-container">
+              <div className="boxes">
+                Loren Ipsum
+                <button>Loren</button>
+              </div>
+              <div className="boxes">
+                Loren Ipsum
+                <button>Loren</button>
+              </div>
+              <div className="boxes">
+                Loren Ipsum
+                <button>Loren</button>
+              </div>
+              <div className="boxes">
+                Loren Ipsum
+                <button>Loren</button>
+              </div>
+            </section>
+          <Footer />
+        </main>
       </>
     )
 }
