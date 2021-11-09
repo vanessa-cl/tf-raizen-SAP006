c79e5e42327c74609caedaae9a30525e9bf3d019
Isisnoron <isisbeatrizrn@gmail.com>
2021-11-09 20:28:39 -0300

    page not found and footer

--- tf-raizen/src/App.js
+++ tf-raizen/src/App.js
@@ -3,6 +3,7 @@ import Home from './pages/Home/Home';
 import MyTickets from './pages/MyTickets/MyTickets';
 import './global.scss';
 import Header from './components/Header/Header';
+import NotFound from './components/NotFound/notfound';
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
         <Routes>
           <Route path="/" exact element={<Home />} />
           <Route path="/mytickets" element={<MyTickets />} />
+          <Route path="*" element={<NotFound />} />
+          {/* <Route path="/notfound" element={() => ( <NotFound /> )}	/> */}
         </Routes>
 
     </BrowserRouter>
--- tf-raizen/src/components/Footer/Footer.js
+++ tf-raizen/src/components/Footer/Footer.js
@@ -1,9 +1,38 @@
 import './Footer.scss'
+import cosan from "./img/icon-logo-cosan.png"
+import raizen from "./img/icon-logo-raizen.png"
+
+import comgos from "./img/icon-logo-comgas.png"
+import compass from "./img/icon-logo-compass.png"
+import moove from "./img/icon-logo-moove.png"
+import rumo from "./img/icon-logo-rumo.png"
 
 function Footer() {
     return (
         <footer className="footer">
-            <p>Coisas do footer</p>
+
+            <div class="links-raizen">
+            <a target="_blank" class="links" href="https://www.cosan.com.br/" title="Cosan">
+            <img src={cosan} className="logo-empresas" alt="logo" /> </a>
+
+            <a target="_blank" class="links" href="https://www.raizen.com.br/" title="Raízen">
+            <img src={raizen} className="logo-empresas" alt="logo" />
+            <i class="icon-logo-raizen"> </i></a>
+
+            <a target="_blank" class="links" href="https://www.raizen.com.br/">
+            <img src={comgos} className="logo-empresas" alt="logo" /></a>
+
+            <a target="_blank" class="links" href="https://www.compassbr.com/" title="Compass">
+            <img src={compass} className="logo-empresas" alt="logo" /></a>
+
+            <a target="_blank" class="links" href="https://www.cosan.com.br/moove/" title="Moove">
+            <img src={moove} className="logo-empresas" alt="logo" /></a>
+
+            <a target="_blank" class="links" href="https://rumolog.com/" title="Rumo">
+            <img src={rumo} className="logo-empresas" alt="logo" /></a>
+
+            </div>
+
         </footer>
     )
 }
