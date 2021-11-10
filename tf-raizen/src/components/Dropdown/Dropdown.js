6c62e7cac0bed0909e05af184759091e23b57195
Isisnoron <isisbeatrizrn@gmail.com>
2021-11-10 20:08:14 -0300

    estilizando navbar

--- tf-raizen/src/components/Dropdown/Dropdown.js
+++ tf-raizen/src/components/Dropdown/Dropdown.js
@@ -1,30 +1,29 @@
 import React, { useState } from "react";
-import './Dropdown.scss';
-import bell from '../../images/bell.png'
+import "./Dropdown.scss";
+import bell from "../../images/bell.png";
 
 const Dropdown = () => {
-    const [openDropdown, setOpenDropdown] = useState(false);
+  const [openDropdown, setOpenDropdown] = useState(false);
 
-    return (
-        <nav className='dropdown-nav' >
-            <img className="bell" src={bell} alt="bell" onClick={() => {
-            openDropdown === false ?
-                setOpenDropdown(true)
-                : setOpenDropdown(false);
-        }}/>
-            <div className={`nav-links ${openDropdown === true ? ' show' : ''}`}>
-                <button>Menu</button>
-                <button>Cozinha</button>
-                <button>Finalizados</button>
-                <button>Logout</button>
+  return (
+    <nav className="dropdown-nav">
+      <img
+        className="bell"
+        src={bell}
+        alt="bell"
+        onClick={() => {
+          openDropdown === false
+            ? setOpenDropdown(true)
+            : setOpenDropdown(false);
+        }}
+      />
+      <div className={`nav-links ${openDropdown === true ? " show" : ""}`}>
+          <div class="bar"> 
+          <p class="pendencies"> Pendências </p></div>
+        <p class="notifications"> Reserva de assento no micro-ônibus concluída com sucesso!</p>
             </div>
-        </nav>
-    );
-
-
-
-
-}
+    </nav>
+  );
+};
 
 export default Dropdown;
-
